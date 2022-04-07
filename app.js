a29f4e8a0c365d76818ec6019f1e65c1dbff7f5a
Herman Chien <Herman@fet.vue>
2022-04-07 11:59:07 +0800

    start

---
+++ app.js
@@ -0,0 +1,14 @@
+Vue.createApp({
+    data() { 
+        return { 
+            courses: [],
+            courseContent:"initial"
+        } 
+    },
+    computed: {},
+    methods: {
+        addCourse(){
+            this.courses.push(this.courseContent)
+        }
+    }
+}).mount("#app")
