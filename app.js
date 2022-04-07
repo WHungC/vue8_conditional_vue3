e6a4a1ccb3efe5e09522b0d118c26cffe30ee75c
Herman Chien <Herman@fet.vue>
2022-04-07 13:54:31 +0800

    lots way v-for

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 Vue.createApp({
-    data() { 
-        return { 
-            courses: [],
-            courseContent:"initial"
-        } 
-    },
-    computed: {},
-    methods: {
-        addCourse(){
-            this.courses.push(this.courseContent)
-        }
-    }
+	data() { 
+		return { 
+			courses: [],
+			courseContent:"initial",
+			poop: {
+				name: "POOP",
+				duration: 35,
+				period: ['Sat', 'Sun'],
+				location: ['Taipei', 'Remote'],
+				instructor: {
+					name: "Mark",
+					location: 'Taipei'
+				}
+			}
+		} 
+	},
+	computed: {},
+	methods: {
+		addCourse(){
+			this.courses.push(this.courseContent)
+		},
+		removeCourse(i){
+			this.courses.splice(i,1)
+		}
+	}
 }).mount("#app")
